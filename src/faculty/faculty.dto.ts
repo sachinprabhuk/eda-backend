a8b8e02646cc20abcf3d5cafd49117f43c78d6d8
sachinprabhuk <prabhusachin44@gmail.com>
2019-07-04 12:13:02 +0530

    updates

--- src/faculty/faculty.dto.ts
+++ src/faculty/faculty.dto.ts
@@ -1,36 +1,33 @@
-import { Slot } from "src/entities/Slot.entity";
+import { Slot } from '../entities/Slot.entity';
 
 export class slotsResp {
-	id: string
-	date: Date
-	total: number
-	remaining: number
+  id: string;
+  date: Date;
+  total: number;
+  remaining: number;
 }
 
-
 export class SlotSelectionError {
-	message: string
-	data: any
-	constructor(message: string, data: any) {
-		this.message = message;
-		this.data = data;
-	}
+  message: string;
+  data: any;
+  constructor(message: string, data: any) {
+    this.message = message;
+    this.data = data;
+  }
 }
 
 export class SlotSelectionResp {
-	errors: Array<SlotSelectionError>
-	allotedSlots: Slot[]
-	
-	constructor() {
-		this.errors = [];
-		this.allotedSlots = [];
-	}
+  errors: Array<SlotSelectionError>;
+  allotedSlots: Slot[];
 
-	updateResp(data: SlotSelectionError | Slot) {
-		if(!data) return;
-		if(data instanceof SlotSelectionError)
-			this.errors.push(data);
-		else
-			this.allotedSlots.push(data);
-	}
+  constructor() {
+    this.errors = [];
+    this.allotedSlots = [];
+  }
+
+  updateResp(data: SlotSelectionError | Slot) {
+    if (!data) return;
+    if (data instanceof SlotSelectionError) this.errors.push(data);
+    else this.allotedSlots.push(data);
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,8 @@ import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-declare const module: any;
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT);
-
-  if (module.hot) {
-    module.hot.accept();
-    module.hot.dispose(() => app.close());
-  }
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
